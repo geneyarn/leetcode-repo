314928454a436a2544ea826678cb4381afe55114
gaojinyang <gaojinyang02@meituan.com>
2025-12-30 20:30:22 +0800

    1

--- dp_hot/solution_5.py
+++ dp_hot/solution_5.py
@@ -1,20 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         m = len(s)
-        dp = [[False] * m for _ in range(m)]
+        dp = [[False] * (m) for _ in range(m)]
         for i in range(m):
             dp[i][i] = True
+        mxLen = 1
         start = 0
-        length = 1
-        for i in range(m - 1, -1, -1):
+        for i in range(m - 2, -1, -1):
             for j in range(i + 1, m):
-                if s[i] == s[j] and (dp[i + 1][j - 1] or j - i <= 2):
+                if s[i] == s[j] and (dp[i + 1][j - 1] or j - i <= 1):
                     dp[i][j] = True
-                    if j - i + 1 > length:
+                    if j - i + 1 >= mxLen:
                         start = i
-                        length = j - i + 1
-        return s[start: start + length]
+                        mxLen = j - i + 1
+
+        return s[start:start + mxLen]
 
 
-result = Solution().longestPalindrome('babad')
+# result = Solution().longestPalindrome('babad')
+result = Solution().longestPalindrome('cbbd')
 print(result)
