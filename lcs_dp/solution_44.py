2bcf6d96687c254e4b71a44bca4bc85c5ed3f632
yarn <geneyarn@gmail.com>
2026-01-07 09:06:23 +0800

    binary search

--- lcs_dp/solution_44.py
+++ lcs_dp/solution_44.py
@@ -1,3 +1,15 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
+
+        def dp(i: int, j: int) -> bool:
+            if i >= m and j >= n:
+                return True
+
+            if i < m and j >= n:
+                return False
+
+            if s[i] == p[j] or p[j] == '?':
+                return dp(i + 1, j + 1)
+            if p[j] == '*':
+                return dp(i + 1, j + 1) or dp(i, j + 1)
