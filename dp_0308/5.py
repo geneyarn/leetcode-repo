78ab44de777c06579ef1fd410f68f543cd0de370
yarn <geneyarn@gmail.com>
2026-03-16 21:52:50 +0800

    binary search

---
+++ dp_0308/5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        m = len(s)
+        dp = [[False] * m for _ in range(m)]
+        for i in range(m):
+            dp[i][i] = True
+
+        length = 1
+        start = 0
+        for i in range(m - 2, -1, -1):
+            for j in range(i + 1, m):
+                if s[i] == s[j] and (dp[i + 1][j - 1] or j - i < 2):
+                    dp[i][j] = True
+
+                if dp[i][j] and (j - i + 1) > length:
+                    length = j - i + 1
+                    start = i
+        return s[start:start + length]
+
+
+# result = Solution().longestPalindrome('babad')
+result = Solution().longestPalindrome('a')
+# result = Solution().longestPalindrome('cbbd')
+print(result)
