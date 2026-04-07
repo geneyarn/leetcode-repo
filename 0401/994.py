70d02e8b78b3e8da8ad8d8c434de2aceee94be73
yarn <geneyarn@gmail.com>
2026-04-07 09:12:17 +0800

    binary search

--- 0401/994.py
+++ 0401/994.py
@@ -4,3 +4,17 @@ from typing import List
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
+
+        fresh = 0
+        q = []
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    fresh += 1
+                elif grid[i][j] == 2:
+                    q.append((i, j))
+
+        ans = 0
+        while q and fresh > 0:
+            sz = len(q)
